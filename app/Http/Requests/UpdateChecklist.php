949d0bf69feb60e5d503756449ef78fb755e7fdd
WalaaAlbaghdadi <walaa.albaghdadi@hotmail.com>
2023-05-12 18:43:10 +0300

    add form request validation on update process

---
+++ app/Http/Requests/UpdateChecklist.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Checklist;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateChecklist extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $checklist = Checklist::find($this->checklist_id);
+
+        return Auth::user()->id === $checklist->created_by;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|unique:checklists|max:255',
+            'description' => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'A name is required',
+            'name.unique' => 'A checklist name should be unique',
+            'name.max' => 'A checklist name exceeds the max limit',
+            'description.required'  => 'A description is required',
+        ];
+    }
+}
