439f0086be1261a684fb35710a9115ed79f602c5
WalaaAlbaghdadi <walaa.albaghdadi@hotmail.com>
2023-05-13 15:31:38 +0300

    Fix logout problem

--- resources/views/checklists/index.blade.php
+++ resources/views/checklists/index.blade.php
@@ -11,7 +11,7 @@
         <div class="card-header">
             <h3 class="card-title">
                 <i class="ion ion-clipboard mr-1"></i>
-                Task List{{ Lang::get('checklists.checklist') }}
+                {{ Lang::get('checklists.checklist') }} Datatable
             </h3>
         </div>
         <div class="card-body">
@@ -72,7 +72,7 @@
 @section('scripts')
 {{--@include('JS.JSchecklist')--}}
 <script src="{{asset('js/pages/checklist.js')}}"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+{{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>--}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
 <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
